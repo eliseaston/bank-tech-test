3085ead0af30b66e6876dfd55e28cb87a33292a2
ellieSMASH <eliseaston@gmail.com>
2019-12-16 15:36:23 +0000

    statement from acceptance criteria prints, but with today's date

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -1,16 +1,17 @@
 class Transaction
 
-  def initialize(trans_type, amount, balance)
+  def initialize(trans_type, amount, balance, date=Time.now)
     @trans_type = trans_type
     @amount = amount
     @balance = balance
+    @date = date.strftime("%d/%m/%Y")
   end
 
   def create
     if @trans_type == "withdrawal"
-      transaction_data = [Time.now.strftime("%d/%m/%Y "), " ", " %.2f " % @amount, " %.2f" % @balance]
+      transaction_data = ["#{@date} ", " ", " %.2f " % @amount, " %.2f" % @balance]
     elsif @trans_type == "deposit"
-      transaction_data = [Time.now.strftime("%d/%m/%Y "), " %.2f " % @amount, " ", " %.2f" % @balance]
+      transaction_data = ["#{@date} ", " %.2f " % @amount, " ", " %.2f" % @balance]
     end
     transaction = transaction_data.join("||")
   end
