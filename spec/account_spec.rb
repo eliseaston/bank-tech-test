59e3de4694196ce188d0a67a15fca1acd7384ed6
ellieSMASH <eliseaston@gmail.com>
2019-12-16 12:34:04 +0000

    refactor tests to define my_account before all tests

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,20 +1,18 @@
 require 'account'
 
 describe Account do
+  let(:my_account) { described_class.new }
 
   it 'prints an empty statement when there is no bank activity' do
-    my_account = Account.new
     expect(my_account.print_statement).to eq("date || credit || debit || balance")
   end
 
   it 'prints one line of activity on the statement when user deposits 100' do
-    my_account = Account.new
     my_account.deposit(100)
     expect(my_account.print_statement).to eq("date || credit || debit || balance\n16/12/2019 || 100.00 ||  || 100.00")
   end
 
   it 'prints one line of activity on statement when user withdraws 100' do
-    my_account = Account.new
     my_account.withdraw(100)
     expect(my_account.print_statement).to eq("date || credit || debit || balance\n16/12/2019 || || 100.00 || -100.00")
   end
