ad2f5e63186119c542e066b0c4c2ede298292d6a
ellieSMASH <eliseaston@gmail.com>
2019-12-16 13:50:15 +0000

    fix format of printed statement to match acceptance criteria

--- lib/account.rb
+++ lib/account.rb
@@ -7,15 +7,15 @@ class Account
 
   def deposit(amount)
     @balance += amount
-    transaction_data = [Time.now.strftime("%d/%m/%Y"), "%.2f" % amount, "", "%.2f" % @balance]
-    transaction = transaction_data.join(" || ")
+    transaction_data = [Time.now.strftime("%d/%m/%Y "), " %.2f " % amount, " ", " %.2f" % @balance]
+    transaction = transaction_data.join("||")
     @statement.push(transaction)
   end
 
   def withdraw(amount)
     @balance -= amount
-    transaction_data = [Time.now.strftime("%d/%m/%Y"), "", "%.2f" % amount, "%.2f" % @balance]
-    transaction = transaction_data.join(" || ")
+    transaction_data = [Time.now.strftime("%d/%m/%Y "), " ", " %.2f " % amount, " %.2f" % @balance]
+    transaction = transaction_data.join("||")
     @statement.push(transaction)
   end
 
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -9,18 +9,18 @@ describe Account do
 
   it 'prints one line of activity on the statement when user deposits 100' do
     my_account.deposit(100)
-    expect(my_account.print_statement).to eq("date || credit || debit || balance\n16/12/2019 || 100.00 ||  || 100.00")
+    expect(my_account.print_statement).to eq("date || credit || debit || balance\n16/12/2019 || 100.00 || || 100.00")
   end
 
   it 'prints one line of activity on statement when user withdraws 100' do
     my_account.withdraw(100)
-    expect(my_account.print_statement).to eq("date || credit || debit || balance\n16/12/2019 ||  || 100.00 || -100.00")
+    expect(my_account.print_statement).to eq("date || credit || debit || balance\n16/12/2019 || || 100.00 || -100.00")
   end
 
   it 'prints two lines of activity on statement when user deposits and then withdraws' do
     my_account.deposit(100)
     my_account.withdraw(100)
-    expect(my_account.print_statement).to eq("date || credit || debit || balance\n16/12/2019 || 100.00 ||  || 100.00\n16/12/2019 ||  || 100.00 || 0.00")
+    expect(my_account.print_statement).to eq("date || credit || debit || balance\n16/12/2019 || 100.00 || || 100.00\n16/12/2019 || || 100.00 || 0.00")
   end
 
 end
