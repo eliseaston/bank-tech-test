8858e5d9a0471e5278b10598531936ab4a403d21
ellieSMASH <eliseaston@gmail.com>
2019-12-16 14:11:15 +0000

    Transaction class creates correctly formatted transaction for both withdrawals and deposits

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -1,12 +1,17 @@
 class Transaction
 
-  def initialize(amount, balance)
+  def initialize(trans_type, amount, balance)
+    @trans_type = trans_type
     @amount = amount
     @balance = balance
   end
 
   def create
-    transaction_data = [Time.now.strftime("%d/%m/%Y "), " ", " %.2f " % @amount, " %.2f" % @balance]
+    if @trans_type == "withdrawal"
+      transaction_data = [Time.now.strftime("%d/%m/%Y "), " ", " %.2f " % @amount, " %.2f" % @balance]
+    elsif @trans_type == "deposit"
+      transaction_data = [Time.now.strftime("%d/%m/%Y "), " %.2f " % @amount, " ", " %.2f" % @balance]
+    end
     transaction = transaction_data.join("||")
   end
 
--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -2,9 +2,14 @@ require 'transaction'
 
 describe Transaction do
 
-  it 'creates a new transaction in the correct || format for the statement' do
-    withdrawal = Transaction.new(100, 0)
+  it 'creates a new withdrawal transaction in the correct || format for the statement' do
+    withdrawal = Transaction.new("withdrawal", 100, 0)
     expect(withdrawal.create).to eq("16/12/2019 || || 100.00 || 0.00")
+  end
+
+  it 'creates a new deposit transaction in the correct || format for the statement' do
+    deposit = Transaction.new("deposit", 100, 100)
+    expect(deposit.create).to eq("16/12/2019 || 100.00 || || 100.00")
   end
 
 end
