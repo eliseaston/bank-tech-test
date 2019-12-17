00bd48ba69444748468f38dbf4675301efa11856
ellieSMASH <eliseaston@gmail.com>
2019-12-17 09:17:24 +0000

    SimpleCov gem. Update tests to always expect today's date

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -3,31 +3,35 @@ require 'account'
 describe Account do
   let(:my_account) { described_class.new }
 
+  before(:each) do
+    @today_date = Time.now.strftime("%d/%m/%Y")
+  end
+
   it 'prints an empty statement when there is no bank activity' do
     expect(my_account.print_statement).to eq("date || credit || debit || balance\n")
   end
 
   it 'prints one line of activity on the statement when user deposits 100' do
     my_account.deposit(100)
-    expect(my_account.print_statement).to eq("date || credit || debit || balance\n16/12/2019 || 100.00 || || 100.00")
+    expect(my_account.print_statement).to eq("date || credit || debit || balance\n#{@today_date} || 100.00 || || 100.00")
   end
 
   it 'prints one line of activity on statement when user withdraws 100' do
     my_account.withdraw(100)
-    expect(my_account.print_statement).to eq("date || credit || debit || balance\n16/12/2019 || || 100.00 || -100.00")
+    expect(my_account.print_statement).to eq("date || credit || debit || balance\n#{@today_date} || || 100.00 || -100.00")
   end
 
   it 'prints two lines of activity on statement when user deposits and then withdraws' do
     my_account.deposit(100)
     my_account.withdraw(100)
-    expect(my_account.print_statement).to eq("date || credit || debit || balance\n16/12/2019 || || 100.00 || 0.00\n16/12/2019 || 100.00 || || 100.00")
+    expect(my_account.print_statement).to eq("date || credit || debit || balance\n#{@today_date} || || 100.00 || 0.00\n#{@today_date} || 100.00 || || 100.00")
   end
 
   it 'prints statement with multiple deposits and withdrawal, with today date' do
     my_account.deposit(1000)
     my_account.deposit(2000)
     my_account.withdraw(500)
-    expect(my_account.print_statement).to eq("date || credit || debit || balance\n16/12/2019 || || 500.00 || 2500.00\n16/12/2019 || 2000.00 || || 3000.00\n16/12/2019 || 1000.00 || || 1000.00")
+    expect(my_account.print_statement).to eq("date || credit || debit || balance\n#{@today_date} || || 500.00 || 2500.00\n#{@today_date} || 2000.00 || || 3000.00\n#{@today_date} || 1000.00 || || 1000.00")
   end
 
 end
--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -2,14 +2,18 @@ require 'transaction'
 
 describe Transaction do
 
+  before(:each) do
+    @today_date = Time.now.strftime("%d/%m/%Y")
+  end
+
   it 'creates a new withdrawal transaction in the correct || format for the statement' do
     withdrawal = Transaction.new("withdrawal", 100, 0)
-    expect(withdrawal.format).to eq("16/12/2019 || || 100.00 || 0.00")
+    expect(withdrawal.format).to eq("#{@today_date} || || 100.00 || 0.00")
   end
 
   it 'creates a new deposit transaction in the correct || format for the statement' do
     deposit = Transaction.new("deposit", 100, 100)
-    expect(deposit.format).to eq("16/12/2019 || 100.00 || || 100.00")
+    expect(deposit.format).to eq("#{@today_date} || 100.00 || || 100.00")
   end
 
 end
